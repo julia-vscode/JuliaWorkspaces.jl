16f6b850bf43cf027d015bca3bca08675232c47f
David Anthoff <anthoff@berkeley.edu>
2023-02-13 09:25:33 -0800

    Restrict test item detection to Julia 1.6+

--- test/test_testitems.jl
+++ test/test_testitems.jl
@@ -237,7 +237,11 @@ end
     file1_uri = filepath2uri(joinpath(@__DIR__, "data", "project_detection", "TestPackage2", "src", "TestPackage2.jl"))
     file2_uri = filepath2uri(joinpath(@__DIR__, "data", "project_detection", "TestPackage3", "src", "TestPackage3.jl"))
     file3_uri = filepath2uri(joinpath(@__DIR__, "data", "project_detection", "TestPackage4", "src", "TestPackage4.jl"))
-    @test jw._testitems[file1_uri][1].project_uri == filepath2uri(joinpath(@__DIR__, "data", "project_detection", "TestPackage2"))
-    @test jw._testitems[file2_uri][1].project_uri == filepath2uri(joinpath(@__DIR__, "data", "project_detection"))
-    @test jw._testitems[file3_uri][1].project_uri === nothing
+
+    # TODO Figure out whether we can relax this
+    if VERSION >= v"1.6"
+        @test jw._testitems[file1_uri][1].project_uri == filepath2uri(joinpath(@__DIR__, "data", "project_detection", "TestPackage2"))
+        @test jw._testitems[file2_uri][1].project_uri == filepath2uri(joinpath(@__DIR__, "data", "project_detection"))
+        @test jw._testitems[file3_uri][1].project_uri === nothing
+    end
 end
