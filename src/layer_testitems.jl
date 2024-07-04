e71ed88fa0fa6fc7c76b72181bc67bb38962a396
David Anthoff <anthoff@berkeley.edu>
2024-07-04 10:52:55 +0200

    Fix a bug

--- src/layer_testitems.jl
+++ src/layer_testitems.jl
@@ -232,7 +232,7 @@ end
 Salsa.@derived function derived_all_testitems(rt)
     files = derived_julia_files(rt)
 
-    res = Dict{URI,@NamedTuple(testitems::Vector{TestItemDetailt},testsetups::Vector{TestSetupDetail},testerrors::Vector{TestErrorDetail})}(
+    res = Dict{URI,@NamedTuple{testitems::Vector{TestItemDetail},testsetups::Vector{TestSetupDetail},testerrors::Vector{TestErrorDetail}}}(
         uri => derived_testitems(rt, uri)
         for uri in files
     )
