3e085d222efe5ef57ab7385b0235d43587b123ef
David Anthoff <anthoff@berkeley.edu>
2024-07-16 00:13:21 +0200

    Add test failures to diagnostics

--- src/layer_diagnostics.jl
+++ src/layer_diagnostics.jl
@@ -1,3 +1,5 @@
 function get_diagnostic(jw::JuliaWorkspace, uri::URI)
-    return derived_julia_syntax_diagnostics(jw.runtime, uri)
+    syntax_diagnostics = derived_julia_syntax_diagnostics(jw.runtime, uri)
+    testitem_diagnostics = [Diagnostic(i.range, :error, i.message, "Testitem") for i in derived_testitems(jw.runtime, uri).testerrors]
+    return [syntax_diagnostics...; testitem_diagnostics...]
 end
