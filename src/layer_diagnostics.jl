0d3607a11c6379692f1efe044feeecfc25734242
David Anthoff <anthoff@berkeley.edu>
2024-07-17 19:36:46 +0200

    Add linting options

--- src/layer_diagnostics.jl
+++ src/layer_diagnostics.jl
@@ -6,6 +6,27 @@ Salsa.@derived function derived_lintconfig_files(rt)
     return [file for file in files if file.scheme=="file" && is_path_lintconfig_file(uri2filepath(file))]
 end
 
+
+Salsa.@derived function derived_lintconfig_diagnostics(rt, uri)
+    toml_content = derived_toml_syntax_tree(rt, uri)
+
+    res = Diagnostic[]
+
+    valid_lint_configs = ["syntax-errors", "syntax-warnings", "testitem-errors", "toml-syntax-errors", "lint-config-errors"]
+
+    for (k,v) in pairs(toml_content)
+        if !(k in valid_lint_configs)
+            push!(res, Diagnostic(1:1, :error, "Invalid lint configuration $k.", "JuliaWorkspaces.jl"))
+        end
+
+        if !(v isa Bool)
+            push!(res, Diagnostic(1:1, :error, "Invalid lint configuration value for $k, ony `true` or `false` are valid.", "JuliaWorkspaces.jl"))
+        end
+    end
+
+    return res
+end
+
 Salsa.@derived function derived_lint_configuration(rt, uri)
     config_files = derived_lintconfig_files(rt)
 
@@ -32,27 +53,36 @@ Salsa.@derived function derived_diagnostics(rt, uri)
 
     results = Diagnostic[]
 
-    if get(lint_config, "syntax-errors", true) == true || get(lint_config, "syntax-errors", false) == true
+    if is_path_julia_file(uri2filepath(uri)) && get(lint_config, "syntax-errors", true) == true || get(lint_config, "syntax-warnings", false) == true
         syntax_diagnostics = derived_julia_syntax_diagnostics(rt, uri)
 
         if get(lint_config, "syntax-errors", true) == true
             append!(results, i for i in syntax_diagnostics if i.severity==:error)
         end
 
-        if get(lint_config, "syntax-errors", false) == true
+        if get(lint_config, "syntax-warnings", false) == true
             append!(results, i for i in syntax_diagnostics if i.severity==:warning)
         end
     end
 
-    if get(lint_config, "testitem-errors", true) == true
+    if is_path_julia_file(uri2filepath(uri)) && get(lint_config, "testitem-errors", true) == true
         tis = derived_testitems(rt, uri)
         append!(results, Diagnostic(i.range, :error, i.message, "Testitem") for i in tis.testerrors)
     end
 
+    if (is_path_lintconfig_file(uri2filepath(uri)) || is_path_project_file(uri2filepath(uri)) || is_path_manifest_file(uri2filepath(uri)) ) && get(lint_config, "toml-syntax-errors", true) == true
+        toml_syntax_errors = derived_toml_syntax_diagnostics(rt, uri)
+        append!(results, toml_syntax_errors)
+    end
+
+    if is_path_lintconfig_file(uri2filepath(uri)) && get(lint_config, "lint-config-errors", true) == true
+        lint_config_errors = derived_lintconfig_diagnostics(rt, uri)
+        append!(results, lint_config_errors)
+    end
 
     return results
 end
 
-function get_diagnostic(jw::JuliaWorkspace, uri::URI)    
+function get_diagnostic(jw::JuliaWorkspace, uri::URI)
     return derived_diagnostics(jw.runtime, uri)
 end
--- src/layer_syntax_trees.jl
+++ src/layer_syntax_trees.jl
@@ -1,6 +1,6 @@
 Salsa.@derived function derived_julia_parse_result(rt, uri)
     tf = input_text_file(rt, uri)
-    
+
     content = tf.content.content
 
     stream = JuliaSyntax.ParseStream(content; version=VERSION)
@@ -31,12 +31,30 @@ Salsa.@derived function derived_julia_syntax_diagnostics(rt, uri)
     return diag_results
 end
 
-Salsa.@derived function derived_toml_syntax_tree(rt, uri)
+Salsa.@derived function derived_toml_parse_result(rt, uri)
     tf = input_text_file(rt, uri)
-    
+
     content = tf.content.content
 
-    return Pkg.TOML.parse(content)
+    parse_result = Pkg.TOML.tryparse(content)
+
+    if parse_result isa Pkg.TOML.ParserError
+        return parse_result.table, Diagnostic[Diagnostic(parse_result.pos:parse_result.pos, :error, Base.TOML.format_error_message_for_err_type(parse_result), "TOML.jl")]
+    else
+        return parse_result, Diagnostic[]
+    end
+end
+
+Salsa.@derived function derived_toml_syntax_tree(rt, uri)
+    parse_result = derived_toml_parse_result(rt, uri)
+
+    return parse_result[1]
+end
+
+Salsa.@derived function derived_toml_syntax_diagnostics(rt, uri)
+    parse_result = derived_toml_parse_result(rt, uri)
+
+    return parse_result[2]
 end
 
 function get_julia_syntax_tree(jw::JuliaWorkspace, uri::URI)
