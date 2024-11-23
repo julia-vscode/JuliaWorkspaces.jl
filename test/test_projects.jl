9663118f4871e79c890bb5349dfde1a090624a63
David Anthoff <anthoff@berkeley.edu>
2024-11-22 18:37:19 -0800

    Handle different versions in manifest for stdlib

--- test/test_projects.jl
+++ test/test_projects.jl
@@ -26,28 +26,43 @@
 end
 
 @testitem "Manifest details" begin
-    using UUIDs
-    pkg_root = abspath(joinpath(@__DIR__, "data", "project_detection"))
+    using UUIDs, Pkg
 
-    jw = workspace_from_folders([pkg_root])
+    mktempdir() do root_path
+        cp(joinpath(@__DIR__, "data", "project_detection"), joinpath(root_path, "project_detection"))
 
-    project_uri = first(get_projects(jw))
+        Pkg.activate(joinpath(root_path, "project_detection"))
+        Pkg.develop(PackageSpec(path=joinpath(root_path, "project_detection", "TestPackage3")))
+        Pkg.instantiate()
 
-    project_details = JuliaWorkspaces.derived_project(jw.runtime, project_uri)
+        pkg_root = joinpath(root_path, "project_detection")
 
-    @test haskey(project_details.regular_packages, "JuliaSyntax") === true
-    @test project_details.regular_packages["JuliaSyntax"].name == "JuliaSyntax"
-    @test project_details.regular_packages["JuliaSyntax"].git_tree_sha1 == "c6ca86ffc29b2fbe9423ff275eaa542057725c7f"
-    @test project_details.regular_packages["JuliaSyntax"].uuid == UUID("70703baa-626e-46a2-a12c-08ffd08c73b4")
-    @test project_details.regular_packages["JuliaSyntax"].version == "0.3.0"
-
-    @test haskey(project_details.stdlib_packages, "Dates") === true
-    @test project_details.stdlib_packages["Dates"].name == "Dates"
-    @test project_details.stdlib_packages["Dates"].uuid == UUID("ade2ca70-3891-5945-98fb-dc099432e06a")
-    @test project_details.stdlib_packages["Dates"].version === nothing
-
-    @test haskey(project_details.deved_packages, "TestPackage3") === true
-    @test project_details.deved_packages["TestPackage3"].name == "TestPackage3"
-    @test project_details.deved_packages["TestPackage3"].uuid == UUID("d952f820-d47c-4fa1-a74c-bfd674713277")
-    @test project_details.deved_packages["TestPackage3"].version == "1.0.0"
+        jw = workspace_from_folders([pkg_root])
+    
+        project_uri = first(get_projects(jw))
+    
+        project_details = JuliaWorkspaces.derived_project(jw.runtime, project_uri)
+    
+        @test haskey(project_details.regular_packages, "JuliaSyntax") === true
+        @test project_details.regular_packages["JuliaSyntax"].name == "JuliaSyntax"
+        @test project_details.regular_packages["JuliaSyntax"].git_tree_sha1 == "c6ca86ffc29b2fbe9423ff275eaa542057725c7f"
+        @test project_details.regular_packages["JuliaSyntax"].uuid == UUID("70703baa-626e-46a2-a12c-08ffd08c73b4")
+        @test project_details.regular_packages["JuliaSyntax"].version == "0.3.0"
+    
+        @test haskey(project_details.stdlib_packages, "Dates") === true
+        @test project_details.stdlib_packages["Dates"].name == "Dates"
+        @test project_details.stdlib_packages["Dates"].uuid == UUID("ade2ca70-3891-5945-98fb-dc099432e06a")
+        println("THE VERSION IS ", project_details.stdlib_packages["Dates"].version)
+        if VERSION >= v"1.11.0"
+            @test VersionNumber(project_details.stdlib_packages["Dates"].version).major == VERSION.major
+            @test VersionNumber(project_details.stdlib_packages["Dates"].version).minor == VERSION.minor
+        else
+            @test project_details.stdlib_packages["Dates"].version === nothing
+        end
+    
+        @test haskey(project_details.deved_packages, "TestPackage3") === true
+        @test project_details.deved_packages["TestPackage3"].name == "TestPackage3"
+        @test project_details.deved_packages["TestPackage3"].uuid == UUID("d952f820-d47c-4fa1-a74c-bfd674713277")
+        @test project_details.deved_packages["TestPackage3"].version == "1.0.0"
+    end
 end
