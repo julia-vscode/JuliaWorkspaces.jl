a4feb85030a7470518a4832a0deb4ca0a0e72a16
David Anthoff <anthoff@berkeley.edu>
2026-02-13 15:24:28 -0800

    Add more diagnostics

--- main.jl
+++ main.jl
@@ -6,4 +6,11 @@ add_folder_from_disc!(jw, joinpath(homedir(), ".julia/environments/v1.12"))
 
 JuliaWorkspaces.set_input_active_project!(jw.runtime, JuliaWorkspaces.filepath2uri(joinpath(homedir(), ".julia/environments/v1.12")))
 
-get_diagnostics(jw)
+diags = get_diagnostics(jw)
+
+for (uri,msgs) in pairs(diags)
+    println(" Diags for $(JuliaWorkspaces.uri2filepath(uri)):")
+    for msg in msgs
+        println("  $msg")
+    end
+end
