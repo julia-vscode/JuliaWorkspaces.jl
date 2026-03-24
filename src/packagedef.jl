adbaa64c3e75f9766c685f1ae75cfb81edffa4e2
David Anthoff <anthoff@berkeley.edu>
2026-03-23 17:28:12 -0700

    Move to a packagedef.jl model

---
+++ src/packagedef.jl
@@ -0,0 +1,23 @@
+include("utils.jl")
+
+include("compat.jl")
+
+import Pkg
+
+include("URIs2/URIs2.jl")
+import .URIs2
+using .URIs2: filepath2uri, uri2filepath
+
+using .URIs2: URI, @uri_str
+
+include("exception_types.jl")
+include("types.jl")
+include("sourcetext.jl")
+include("inputs.jl")
+include("layer_files.jl")
+include("layer_syntax_trees.jl")
+include("layer_projects.jl")
+include("layer_testitems.jl")
+include("layer_diagnostics.jl")
+include("fileio.jl")
+include("public.jl")
