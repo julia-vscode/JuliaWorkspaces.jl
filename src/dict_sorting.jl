d72296c6dc51ffd9ba959b46904dd3238a040952
David Anthoff <anthoff@berkeley.edu>
2026-06-18 18:24:52 +0200

    Squashed 'packages-old/v1.5/OrderedCollections/' content from commit cf2bd5f4
    git-subtree-dir: packages-old/v1.5/OrderedCollections
    git-subtree-split: cf2bd5f4fc602ad949a07f0f1e9907cfc363d5f1

---
+++ src/dict_sorting.jl
@@ -0,0 +1,44 @@
+# Sort for dicts
+import Base: sort, sort!
+
+function sort!(d::OrderedDict; byvalue::Bool=false, args...)
+    if d.ndel > 0
+        rehash!(d)
+    end
+
+    if byvalue
+        p = sortperm(d.vals; args...)
+    else
+        p = sortperm(d.keys; args...)
+    end
+    d.keys = d.keys[p]
+    d.vals = d.vals[p]
+    rehash!(d)
+    return d
+end
+
+# Compared to just sorting the underlying OrderedDict, this method calls sort!
+# directly on the keys (no need to sort d.vals::Vector{Nothing}). This saves
+# the allocation of the permutation vector in sortperm, and subsequent
+# allocations of new d.keys and d.vals vectors.
+function sort!(s::OrderedSet; kwargs...)
+    d = s.dict
+    d.ndel > 0 && rehash!(d)
+    sort!(d.keys; kwargs...)
+    rehash!(d)
+    return s
+end
+
+sort(d::Union{OrderedDict,OrderedSet}; args...) = sort!(copy(d); args...)
+
+@deprecate sort(d::Dict; args...) sort!(OrderedDict(d); args...)
+
+function sort(d::LittleDict; byvalue::Bool=false, args...)
+    if byvalue
+        p = sortperm(d.vals; args...)
+    else
+        p = sortperm(d.keys; args...)
+    end
+    return LittleDict(d.keys[p], d.vals[p])
+end
+
